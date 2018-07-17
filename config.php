29c530b58a87674f616fb83e4465a294f0f9051d
ryuring <ryuring@egashiraryuujinoMacBook.local>
2018-07-18 00:00:46 +0900

    設定ファイルを追加

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * CustomContents :  baserCMS Plugin <https://github.com/ryuring/CustomContents>
+ * Copyright (c) ryuring.com
+ *
+ * @copyright		Copyright (c) ryuring.com
+ * @link			http://ryuring.com
+ * @package			CustomContents
+ * @since			baserCMS v 4.1.2
+ * @license			MIT
+ */
+
+$title = __d('baser', 'カスタムコンテンツ');
+$description = __d('baser', '自由なフィールド設定でコンテンツを作成する事ができるプラグイン');
+$author = 'ryuring';
+$url = 'http://ryuring.com';
